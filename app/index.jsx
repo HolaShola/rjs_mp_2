304ccd55cd5a214b9144ee3853ecd2e8d8f94c51
HolaShola <xxjxxx@mail.ru>
2018-03-01 10:17:46 +0300

    add Immutable

--- app/index.jsx
+++ app/index.jsx
@@ -4,8 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import configureStore from './store/configureStore';
+import Immutable, { Iterable } from 'immutable';
 
 const store = configureStore();
+const mapObj = Immutable.Map(
+  {
+    first: "a",
+    second: "b",
+    third: {
+      one: "oneValue",
+      two: "twoValue",
+    },
+  }
+)
+
+console.log(mapObj);
+// don't use inner keys (undefined)
+console.log(mapObj.get("first"));
 
 ReactDOM.render((
   <Provider store={store}>
